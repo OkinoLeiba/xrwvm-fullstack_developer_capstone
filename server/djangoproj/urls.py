c33de0bfc68082660b293742a16c690299aeaab3
OkinoLeiba <okino.leiba@yahoo.com>
2025-06-24 18:15:23 -0400

    add react frontend

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from djangoapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,9 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="Home.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path(route='login', view=views.login_user, name='login'),
+    path(route='logout', view=views.logout_request, name='logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# server/djangoapp/views.py
