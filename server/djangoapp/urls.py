593a9f8a6e1d2233d34a7deeff9b396e422d54cf
OkinoLeiba <okino.leiba@yahoo.com>
2025-07-05 14:16:10 -0400

    add car models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,15 +7,20 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-    path(route='register', view=views.registration, name='register'),
+    path(route="register", view=views.registration, name="register"),
     # path for login
-    path(route='login', view=views.login_user, name='login'),
+    path(route="login", view=views.login_user, name="login"),
 
     #path for logout
-    path(route='logout', view=views.logout_request, name='logout'),
+    path(route="logout", view=views.logout_request, name="logout"),
+
+    # path for getting cars
+    path(route="get_cars", view=views.get_cars, name="get_cars"),
 
     # path for dealer reviews view
+    # path(route="dealer_reviews", view=views.dealer_reviews, name="dealer_reviews"),
 
     # path for add a review view
+    # path(route="add_review", view=views.add_review, name="add_review"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
